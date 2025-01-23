4930c7f3c48e61ee6d0bdb5f9ffe8ddb61fb0fc5
DeaconSteiner <deaconsteiner.it@gmail.com>
2025-01-22 17:39:06 -0700

    Added the necessary assignment files to repo.

---
+++ Lab01/Lab01.cpp
@@ -0,0 +1,15 @@
+// Lab01.cpp
+// Deacon Steiner
+// COSC 1030, Section 12
+// Lab 01
+// 22 Jan 2025
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+    cout << "What do you want, Commander Steiner?" << endl;
+    return 0;
+}
