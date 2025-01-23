3608c69406af55e429b530c37a65bbf72ad4f949
DeaconSteiner <deaconsteiner.it@gmail.com>
2025-01-23 13:16:02 -0700

    Finished assignment procedures

--- Program01/Prog01.cpp
+++ Program01/Prog01.cpp
@@ -3,3 +3,29 @@
 // COSC 1030, Section 12
 // Program 01
 // 23 Jan 2025
+
+#include <iostream>
+
+using std::cout;
+using std:: cin;
+using std::endl;
+
+int main() {
+    int n = 0;
+    int sum = 0;
+
+    cout << "The sum of ";
+
+    while (n < 4)
+    {
+        int value = 0;
+        cin >> value;
+
+        sum += value;
+
+        n++;
+    }
+
+    cout << "is: " << sum << endl;
+    
+}
